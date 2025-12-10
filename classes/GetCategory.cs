e0ecca0a68f61c2de418baf5fa97b30aa4db8940
TalalBalnoob <TalalBalnoob@gmail.com>
2025-12-10 18:33:29 +0300

    some changes

--- classes/GetCategory.cs
+++ classes/GetCategory.cs
@@ -4,18 +4,19 @@ namespace FileOrganizer.classes;
 
 public class Category
 {
-	public static string GetCategory(string extension)
-	{
-		return extension switch
-		{
-			".rpm" or ".deb" or ".exe" or ".appimage" or ".flatpakref" => "Apps",
-			".jpg" or ".png" or ".gif" or ".svg" => "Images",
-			".txt" or ".pdf" or ".docx" => "Docs",
-			".zip" or ".gz" or ".tar" => "Zips",
-			".mp3" or ".wav" => "Music",
-			".mp4" or ".avi" => "Videos",
-			".srt" => "Subtitles",
-			_ => "NONE"
-		};
-	}
+    // this is a change 
+    public static string GetCategory(string extension)
+    {
+        return extension switch
+        {
+            ".rpm" or ".deb" or ".exe" or ".appimage" or ".flatpakref" => "Apps",
+            ".jpg" or ".png" or ".gif" or ".svg" => "Images",
+            ".txt" or ".pdf" or ".docx" => "Docs",
+            ".zip" or ".gz" or ".tar" => "Zips",
+            ".mp3" or ".wav" => "Music",
+            ".mp4" or ".avi" => "Videos",
+            ".srt" => "Subtitles",
+            _ => "NONE"
+        };
+    }
 }
